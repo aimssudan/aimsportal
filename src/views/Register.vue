c9baccf215c466abb526d7210510ef1201e8c791
Marshud <ubatamye@gmail.com>
2022-10-10 15:34:45 +0300

    Register fix

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="page login-page">
+  <main class="page login-page container">
         <div class="col-md-6 offset-md-3">
           <h1 class="text-primary"> Register</h1>
           <nav aria-label="breadcrumb">
@@ -8,68 +8,76 @@
             <li class="breadcrumb-item active" aria-current="page">Register</li>
             </ol>
           </nav>
-          <div class="card">
-            <div class="card-header">
-                <h5>Create your account</h5>
-            </div>
-            <div class="card-body">
-              <div v-if="isLoading" class="spinner-border text-warning" role="status">
-                <span class="visually-hidden">Loading...</span>
+
+          <div class="row">
+            <div class="col-md-1">&nbsp;</div>
+            <div class="col-md-10">
+              <div class="card">
+                <div class="card-header">
+                    <h5>Create your account</h5>
+                </div>
+                <div class="card-body">
+                  <div v-if="isLoading" class="spinner-border text-warning" role="status">
+                    <span class="visually-hidden">Loading...</span>
+                  </div>
+                  <flash-error :hasError="apiErrors" :errors="errors" @dismissError="apiErrors = false"></flash-error>
+                  <div class="row">
+                      <div class="col-md-6 mb-3">
+                          <div class="form-floating">
+                              <input type="text" v-model="form.name" class="form-control" id="floatingInput" placeholder="Joe Doe">
+                              <label for="floatingInput">Name</label>
+                          </div>
+                      </div>
+                      <div class="col-md-6 mb-3">
+                          <div class="form-floating">
+                              <input type="email" v-model="form.email" class="form-control" id="floatingEmail" placeholder="Email Address" required>
+                              <label for="floatingEmail">Email Address</label>
+                          </div>
+                      </div>
+                      <div class="col-md-6 mb-3">
+                          <div class="form-floating">
+                              <input type="password" v-model="form.password" class="form-control" id="floatingPassword" placeholder="Password" required>
+                              <label for="floatingPassword">Password</label>
+                          </div>
+                      </div>
+                      <div class="col-md-6 mb-3">
+                          <div class="form-floating">
+                              <input type="password" v-model="form.password_confirmation" class="form-control" id="floatingPasswordConfirmation" placeholder="Password Confirmation" required>
+                              <label for="floatingPasswordConfirmation">Password Confirmation</label>
+                          </div>
+                      </div>
+                      <div class="col-md-6 mb-3">
+                          <div class="form-floating">
+                            <multiselect @input="updateSelectedOrganisation" class="form-select" v-model="form._o"  track-by="id" label="name" placeholder="Select one" :options="organisations" :searchable="false" :allow-empty="false"></multiselect>
+                              <label for="floatingPassword">Organisation</label>
+                          </div>
+                          <span><router-link :to="{name: 'organisation'}">Add New</router-link></span>
+                      </div>
+                      <div class="col-md-6 mb-3">
+                          <div class="form-floating">
+                              <input type="text"  v-model="form.code" class="form-control" id="floatingOtp" placeholder="OTP CODE" required>
+                              <label for="floatingOtp">OTP CODE</label>
+                          </div>
+                          <span> <button class="btn btn-sm btn-warning mt-2" @click="sendOtp">Resend OTP CODE</button></span>
+                      </div>
+                      <div class="col-md-6 mb-3">
+                          <div class="form-floating">
+                            <select class="form-select" v-model="form.role" placeholder="Role">
+                                <option value="Subscriber" selected>Subscriber</option>
+                                <option value="Contributor">Contributor</option>
+                                <option value="Manager">Manager</option>
+                            </select>
+                              <label for="floatingPassword">Role</label>
+                          </div>
+                      </div>
+                   </div> 
+                   <button class="btn btn-primary" @click="register">Submit</button>
+                </div>
               </div>
-              <flash-error :hasError="apiErrors" :errors="errors" @dismissError="apiErrors = false"></flash-error>
-              <div class="row">
-                  <div class="col-md-6 mb-3">
-                      <div class="form-floating">
-                          <input type="text" v-model="form.name" class="form-control" id="floatingInput" placeholder="Joe Doe">
-                          <label for="floatingInput">Name</label>
-                      </div>
-                  </div>
-                  <div class="col-md-6 mb-3">
-                      <div class="form-floating">
-                          <input type="email" v-model="form.email" class="form-control" id="floatingEmail" placeholder="Email Address" required>
-                          <label for="floatingEmail">Email Address</label>
-                      </div>
-                  </div>
-                  <div class="col-md-6 mb-3">
-                      <div class="form-floating">
-                          <input type="password" v-model="form.password" class="form-control" id="floatingPassword" placeholder="Password" required>
-                          <label for="floatingPassword">Password</label>
-                      </div>
-                  </div>
-                  <div class="col-md-6 mb-3">
-                      <div class="form-floating">
-                          <input type="password" v-model="form.password_confirmation" class="form-control" id="floatingPasswordConfirmation" placeholder="Password Confirmation" required>
-                          <label for="floatingPasswordConfirmation">Password Confirmation</label>
-                      </div>
-                  </div>
-                  <div class="col-md-6 mb-3">
-                      <div class="form-floating">
-                        <multiselect @input="updateSelectedOrganisation" class="form-select" v-model="form._o"  track-by="id" label="name" placeholder="Select one" :options="organisations" :searchable="false" :allow-empty="false"></multiselect>
-                          <label for="floatingPassword">Organisation</label>
-                      </div>
-                      <span><router-link :to="{name: 'organisation'}">Add New</router-link></span>
-                  </div>
-                  <div class="col-md-6 mb-3">
-                      <div class="form-floating">
-                          <input type="text"  v-model="form.code" class="form-control" id="floatingOtp" placeholder="OTP CODE" required>
-                          <label for="floatingOtp">OTP CODE</label>
-                      </div>
-                      <span> <button class="btn btn-sm btn-warning mt-2" @click="sendOtp">Resend OTP CODE</button></span>
-                  </div>
-                  <div class="col-md-6 mb-3">
-                      <div class="form-floating">
-                        <select class="form-select" v-model="form.role" placeholder="Role">
-                            <option value="Subscriber" selected>Subscriber</option>
-                            <option value="Contributor">Contributor</option>
-                            <option value="Manager">Manager</option>
-                        </select>
-                          <label for="floatingPassword">Role</label>
-                      </div>
-                  </div>
-               </div> 
-               <button class="btn btn-primary" @click="register">Submit</button>
+
             </div>
           </div>
+
         </div>
        
     </main>
