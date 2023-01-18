69fbac34da39463c5873a350ba4e626c642c8374
Raymond <raymond.byaruhanga@ingenious.build>
2023-01-18 11:40:18 +0300

    add chartjs

---
+++ src/components/charts/FundingBySectorChart.vue
@@ -0,0 +1,37 @@
+<template>  
+  <canvas id="funding-by-sector-chart"></canvas>
+</template>
+
+<script>
+import  Chart  from 'chart.js/auto';
+
+export default {
+  name: 'FundingBySectorChart',
+  data() {
+    return {
+      chartData: {
+          type: 'doughnut',
+          data: {
+              labels: ['Education', 'Food Sector', 'Security', 'Emergency Relief', 'Not Specified', 'Water sanitation Hygiend'],
+              datasets: [{
+                  label: 'this year',
+                  data: [12, 19, 3, 5, 2, 3],                  
+                  hoverOffset: 4
+              }]
+          },
+          options: {
+              scales: {
+                  y: {
+                      beginAtZero: true
+                  }
+              }
+          }
+      },
+    }
+  },
+  mounted() {
+    const ctx = document.getElementById('funding-by-sector-chart').getContext('2d');
+    new Chart(ctx, this.chartData);
+  }
+}
+</script>
---
+++ src/components/charts/FundingTrendChart.vue
@@ -0,0 +1,37 @@
+<template>  
+  <canvas id="funding-trend-chart"></canvas>
+</template>
+
+<script>
+import  Chart  from 'chart.js/auto';
+
+export default {
+  name: 'FundingTrendChart',
+  data() {
+    return {
+      chartData: {
+          type: 'bar',
+          data: {
+              labels: ['2018', '2019', '2020', '2021', '2022', '2023'],
+              datasets: [{
+                  label: 'past 6 years in USD (m)',
+                  data: [12, 19, 3, 5, 2, 3],                  
+                  borderWidth: 1
+              }]
+          },
+          options: {
+              scales: {
+                  y: {
+                      beginAtZero: true
+                  }
+              }
+          }
+      },
+    }
+  },
+  mounted() {
+    const ctx = document.getElementById('funding-trend-chart').getContext('2d');
+    new Chart(ctx, this.chartData);
+  }
+}
+</script>
