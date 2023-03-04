2a55be98bcfcaa1804037aa7dd875d6e3505a825
ubatamye@gmail.com <ubatamye@gmail.com>
2023-03-04 16:06:29 +0300

    removing line graph

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -29,14 +29,13 @@
 
                 <div class="row mt-4">
                   <h4>In Detail</h4>
-                  <div class="col-md-12">
+                  <!-- <div class="col-md-12">
                     <div class="card" style=" min-height: 50%;">
                       <div class="card-body" >
-                        <!-- Line Graph -->
                         <stacked-line-chart chartTitle="IMPLEMENTED VS TOTAL PROJECTS"></stacked-line-chart>
                       </div>
                     </div>
-                  </div>
+                  </div> -->
                   <div class="col-md-12 mt-3 mb-3">
                     <div class="card"  style=" min-height: 50%;">
                       <div class="card-body">
@@ -141,7 +140,7 @@
                   <hr style="margin-top: 1rem;">
                 </div>
 
-                <div class="row mt-4" id="additional_text">
+                <!-- <div class="row mt-4" id="additional_text">
                   <div class="col-md-12" style="text-align: justify;">
                     <p><b>The Aid Information Management System (AIMS)</b> is your one-stop-shop for all information related to foreign assistance in South Sudan. AIMS records and processes information provided by donors on development activities and related aid flows in the country. The overall objective of this AIMS is to increase aid transparency, strengthen accountability, facilitate coordination and allow for more efficient aid management.</p>
 
@@ -150,7 +149,7 @@
                     <p><b>Access to Aid Information</b><br>
                       AIMS is a publicly-accessible internet tool which is part of the Government of South Sudan's transparency initiative. Anybody can view the information of aid flow in South Sudan, simply by clicking the "Public User" button. However, only authorized user of development partner can enter or update the aid information in the web portal.</p>
                   </div>
-                </div>
+                </div> -->
 
                 <h5 class="mt-3">Feedback</h5>
                 <p><small>Your feedback on data fields and others related issues of this web portal are highly appreciated. Please send your valuable feedback to: <a href="mailto:aimssouthsudan@gmail.com" style="color:blue">AIMS</a></small></p>
@@ -173,14 +172,14 @@ import { mapActions} from 'vuex'
 import flashError from '../components/flashError.vue'
 import ProjectDistributionByState from '../components/maps/ProjectDistributionByState.vue'
 import StackedTrendChart from '../components/charts/StackedTrendChart.vue'
-import StackedLineChart from '../components/charts/StackedLineChart.vue'
+// import StackedLineChart from '../components/charts/StackedLineChart.vue'
 import BarChartReport from '../components/charts/BarChart.vue'
 
 export default {
   name: 'Home',
   components: {
     flashError, ProjectDistributionByState, 
-    StackedTrendChart, StackedLineChart, BarChartReport
+    StackedTrendChart, BarChartReport
     
   },
   data() {
